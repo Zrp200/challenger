0414e30a782b715c7a205a68c97fd7ac0bd8e2a0
Zachary Perlmutter <zrp200@gmail.com>
2015-04-22 21:20:02 -0700

    Update race.rb

--- lib/race.rb
+++ lib/race.rb
@@ -1,10 +1,9 @@
-require "singleton"
 module Challenger
   class Race
-    Human = Orc = new
-    include Singleton
+    Human = new points: 5
+    Orc = Elf = new
     attr_reader :stat
-    def initialize(stat = Hash.new(0))
+    def initialize(stat = Hash.new 0)
       @stat = stat
     end
   end
